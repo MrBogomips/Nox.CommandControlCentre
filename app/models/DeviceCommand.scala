1dceb5c9273b6868a8afae796d950d7ef0534435
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2013-05-02 18:48:59 +0200

    Minor changes

--- app/models/DeviceCommand.scala
+++ app/models/DeviceCommand.scala
@@ -2,6 +2,8 @@ package models
 
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.Logger
+
 
 /** Represents n argument passed to the command */
 case class Argument(val name:String, val value:String)
@@ -14,18 +16,43 @@ object Argument {
 
 /** Represents a command sent to a device */
 case class DeviceCommandRequest(val deviceId: String, val command: String, val arguments: Seq[Argument]) {
-  def sendToDevice(): DeviceCommandResponse = {
-    	DeviceCommandResponseERR() 
+  val creationTime = new java.util.Date
+  lazy val creationTimeISO = {
+    import java.util.TimeZone
+    import java.text.SimpleDateFormat
+    val tz = TimeZone.getTimeZone("UTC");
+    val df = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm'Z'")
+    df.setTimeZone(tz)
+    df.format(creationTime)
+  }
+  def sendToDevice(): DeviceCommandResponse = {	
+    DeviceCommandResponseERR() 
+  }
+  
+  def prepareSendingMessage: String = {
+    val json = Json.toJson(this)
+    json.toString
   }
 }
 object DeviceCommandRequest {
    import Argument._ 
    
-   implicit val jsonFormat = ( 
-	  (__ \ 'deviceId).format[String] and
-      (__ \ 'command).format[String] and
-	  (__ \ 'arguments).format[Seq[Argument]]
-	)(DeviceCommandRequest.apply, unlift(DeviceCommandRequest.unapply))
+	implicit val jsonRead: Reads[DeviceCommandRequest] = ( 
+	  (__ \ 'deviceId).read[String] and
+      (__ \ 'command).read[String] and
+	  (__ \ 'arguments).read[Seq[Argument]]
+	)(DeviceCommandRequest.apply _)
+	
+	implicit val jsonWrite: Writes[DeviceCommandRequest] = ( 
+	  (__ \ 'deviceId).write[String] and
+	  (__ \ 'command).write[String] and
+      (__ \ 'sent_time).write[String] and
+	  (__ \ 'arguments).write[Seq[Argument]]
+	)(unlift(DeviceCommandRequest.unapply2 _))
+	
+   implicit val jsonFormat = Format(jsonRead, jsonWrite) 
+   def unapply2(o: DeviceCommandRequest): Option[(String, String, String, Seq[Argument])] = 
+     if (o != null) Some((o.deviceId,o.command, o.creationTimeISO, o.arguments)) else None
 }
 
 /** Represents a command response sent to the web client */
