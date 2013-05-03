d252b0cfe6838bef13b5a1a18d2fc583535d7cc5
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2013-05-03 11:20:20 +0200

    Logging enabled Minor corrections

--- app/models/DeviceCommand.scala
+++ app/models/DeviceCommand.scala
@@ -1,90 +1,76 @@
 package models
 
+import play.api._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttMessage
-import org.eclipse.paho.client.mqttv3.MqttException
-import org.eclipse.paho.client.mqttv3.IMqttClient 
+import org.eclipse.paho.client.mqttv3._
 import java.util.Date
 
-
-
 /** Represents n argument passed to the command */
-case class Argument(val name:String, val value:String)
+case class Argument(val name: String, val value: String)
 object Argument {
   implicit val jsonFormat = (
-	(__ \ 'name).format[String] and
-    (__ \ 'value).format[String]  
-  )(Argument.apply, unlift(Argument.unapply))
+    (__ \ 'name).format[String] and
+    (__ \ 'value).format[String])(Argument.apply, unlift(Argument.unapply))
 }
 
 /** Represents a command sent to a device */
 case class DeviceCommandRequest(val device: String, val tranId: String, val command: String, val arguments: Seq[Argument]) {
-   
-  val conf = play.Configuration.root()
-  val sentTime = "" 
-   
-  def sendToDevice(jsReq : JsValue): DeviceCommandResponse = {
-   
-    
+  private val conf = play.Configuration.root()
+
+  val creationTime = new java.util.Date
+  lazy val creationTimeISO = {
+    import java.util.TimeZone
+    import java.text.SimpleDateFormat
+    val tz = TimeZone.getTimeZone("UTC");
+    val df = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm'Z'")
+    df.setTimeZone(tz)
+    df.format(creationTime)
+  }
+
+  def sendToDevice(jsReq: JsValue): DeviceCommandResponse = {
     /** Call a MQTT Client to publish the message command */
- 
-     println("device ["+ device + "] command[" + command + "]");
-	 try
-	 {	   
-	   //Setting ClientId to transactionId putted in the message.
-	 
-	   val mqRequestTopic = conf.getString("nox.mqtt.Command.RequestTopic")
-	   
-	   	   
-	   val mqBrokerURI = conf.getString("nox.mqtt.BrokerURI")
-	   
-	   val clientId = MqttClient.generateClientId()
-	   
-	   println("Client created..try to connect ["+ mqBrokerURI + "] on clientId[" + clientId + "]");
-	   
-	   var mqtt = new MqttClient(mqBrokerURI,clientId.substring(0, 22))
+    println("device [" + device + "] command[" + command + "]");
+    try {
+      //Setting ClientId to transactionId putted in the message.
 
-	 
-	   mqtt.connect();
-	   
-	   // Add info in Json and send to MQTT
-	   
-	   //val payload = "{sendtime:""2013-03-19 16:29:34.012345"", expire-s:,maxretry:0, device:""4310516"", command:""OUT1_ACTIVATE"", parameters:[ciao,bubbo,carciofo]}";
-	   
-	   val payload = Json.stringify(jsReq); 
-	   
-	   val message = new MqttMessage(payload.getBytes());
-	   	
-	   mqtt.publish(mqRequestTopic, message)
-	   	
-	   println("Command Sended! Disconnetting..");
-	   mqtt.disconnect()
-	   println("Disconnetted!!");
-	   
-	    DeviceCommandResponseOK("Command Sended successfully")
-	 }
-	catch
-	{
-		case e: Exception => println("exception caught: " + e.printStackTrace());
-		DeviceCommandResponseERR("MAnnaggia la bubbazza")
-	}
+      val mqRequestTopic = conf.getString("nox.mqtt.Command.RequestTopic")
+      val mqBrokerURI = conf.getString("nox.mqtt.BrokerURI")
+      val clientId = MqttClient.generateClientId()
 
-	
-    	 
+      Logger.debug("Client created..try to connect [" + mqBrokerURI + "] on clientId[" + clientId + "]");
+
+      val mqtt = new MqttClient(mqBrokerURI, clientId.substring(0, 22))
+
+      mqtt.connect();
+
+      val payload = Json.stringify(jsReq);
+
+      val message = new MqttMessage(payload.getBytes());
+
+      mqtt.publish(mqRequestTopic, message)
+
+      println("Command Sended! Disconnetting..");
+      mqtt.disconnect()
+      println("Disconnetted!!");
+
+      DeviceCommandResponseOK("Command Sended successfully")
+    } catch {
+      case e: Exception =>
+        Logger.error("exception caught: " + e.printStackTrace());
+        DeviceCommandResponseERR("MAnnaggia la bubbazza")
+    }
+
   }
 }
 object DeviceCommandRequest {
-   import Argument._ 
-   
-   implicit val jsonFormat = ( 
-	  (__ \ 'device).format[String] and
-	  (__ \ 'tranId).format[String] and
-      (__ \ 'command).format[String] and
-	  (__ \ 'arguments).format[Seq[Argument]]
-	)(DeviceCommandRequest.apply, unlift(DeviceCommandRequest.unapply))
+  import Argument._
+
+  implicit val jsonFormat = (
+    (__ \ 'device).format[String] and
+    (__ \ 'tranId).format[String] and
+    (__ \ 'command).format[String] and
+    (__ \ 'arguments).format[Seq[Argument]])(DeviceCommandRequest.apply, unlift(DeviceCommandRequest.unapply))
 }
 
 /** Represents a command response sent to the web client */
@@ -92,19 +78,18 @@ class DeviceCommandResponse(val status: String, val description: String)
 object DeviceCommandResponse {
   implicit val jsonFormat = (
     (__ \ 'status).format[String] and
-    (__ \ 'description).format[String]
-  )(DeviceCommandResponse.apply, unlift(DeviceCommandResponse.unapply))
-  
+    (__ \ 'description).format[String])(DeviceCommandResponse.apply, unlift(DeviceCommandResponse.unapply))
+
   def apply(status: String, description: String = ""): DeviceCommandResponse = {
-	require(status != null, "status cannot be null")
+    require(status != null, "status cannot be null")
     status match {
-     case "OK"  => DeviceCommandResponseOK(description)
-     case "ERR" => DeviceCommandResponseERR(description)
-     case _ 	=> new DeviceCommandResponse(status, description)
-   }
+      case "OK" => DeviceCommandResponseOK(description)
+      case "ERR" => DeviceCommandResponseERR(description)
+      case _ => new DeviceCommandResponse(status, description)
+    }
   }
-  
-  def unapply(o: DeviceCommandResponse): Option[(String, String)] = if (o != null) Some((o.status,o.description)) else None
+
+  def unapply(o: DeviceCommandResponse): Option[(String, String)] = if (o != null) Some((o.status, o.description)) else None
 }
 
 class DeviceCommandResponseOK(description: String) extends DeviceCommandResponse("OK", description)
