bca0841c3888da848d6ad1baaf088aa144f855d5
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2013-07-17 14:59:52 +0200

    REFACTOR: Disposable pattern removed

--- app/notifications/package.scala
+++ app/notifications/package.scala
@@ -2,17 +2,18 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 import mqtt.SimpleClient
-import patterns.disposable._
+import resource._
 
 package object notifications {
   private val conf = play.Configuration.root()
-  def notifyDeviceChangeConfiguration(deviceName: String) = WithDisposition(new SimpleClient) { mqtt =>
-    mqtt.connect
-    val json = Json.obj(
-      "device" -> "server",
-      "command" -> "reset-device",
-      "parameters" -> List(deviceName))
-    val mqRequestTopic = conf.getString("nox.mqtt.Command.RequestTopic")
-    mqtt.publish(mqRequestTopic, json)
-  }
+  def notifyDeviceChangeConfiguration(deviceName: String) = //WithDisposition(new SimpleClient) { mqtt =>
+    for (mqtt <- managed(new SimpleClient)) {
+      mqtt.connect
+      val json = Json.obj(
+        "device" -> "server",
+        "command" -> "reset-device",
+        "parameters" -> List(deviceName))
+      val mqRequestTopic = conf.getString("nox.mqtt.Command.RequestTopic")
+      mqtt.publish(mqRequestTopic, json)
+    }
 }
