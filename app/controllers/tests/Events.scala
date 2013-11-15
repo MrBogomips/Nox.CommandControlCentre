b1ee67f314cedf304b400924cbff63c2aa8f3dd9
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2013-11-15 17:25:25 +0100

    FIX: Demo mode allow usage of Events Tester

--- app/controllers/tests/Events.scala
+++ app/controllers/tests/Events.scala
@@ -13,13 +13,14 @@ object Events extends Secured with ProvidesViewContext {
 
   def index = WithAuthentication { (user, request) =>
     play.api.Play.current.mode match {
-      case Mode.Dev | Mode.Test =>
+      //case Mode.Dev | Mode.Test =>
+      case _ =>
         implicit val r = request
         implicit val u = user
         val wsUrl = controllers.routes.Events.channel.webSocketURL()
         Ok(views.html.tests.events.index(user, wsUrl))
-      case _ =>
-        NotFound("")
+      //case _ =>
+        //NotFound("")
     }
 
   }
